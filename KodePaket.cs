27fc37b07e8a5b5c5ac772906c4bf142dd03bdbf
Zfanz12 <zulfanfalah@gmail.com>
2026-03-28 14:15:06 +0700

    second commit

---
+++ KodePaket.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Modul4_103022400076
+{
+    internal class KodePaket
+    {
+        public string getKodePaket(string namaPaket)
+        {
+            Dictionary<string, string> tabel = new Dictionary<string, string>() {
+                {"Basic", "P201" },
+                {"Standard", "P202" },
+                {"Premium", "P203" },
+                {"Unlimited", "P204" },
+                {"Gaming", "P205" },
+                {"Streaming", "P206" },
+                {"Family", "P207" },
+                {"Business", "P208" },
+                {"Student", "P209" },
+                {"Traveler", "P210" }
+            };
+            if (tabel.ContainsKey(namaPaket))
+                return tabel[namaPaket];
+            else
+                return "Paket tidak ditemukan";
+        }
+    }
+}
