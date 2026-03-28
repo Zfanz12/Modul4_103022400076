86d8f964d85091c6b15ea3688652ff3e0d1b8dc9
Zfanz12 <zulfanfalah@gmail.com>
2026-03-28 14:52:37 +0700

    final commit

---
+++ MesinKopi.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Modul4_103022400076
+{
+    internal class MesinKopi
+    {
+        public enum State
+        {
+            OFF,
+            STANDBY,
+            BREWING,
+            MAINTENANCE
+        }
+        private State currentState = State.OFF;
+        public State GetState() => currentState;
+
+        public void TurnOn()
+        {
+            if (currentState == State.OFF)
+            {
+                Console.WriteLine("Mesin Off berubah menjadi Standby");
+                currentState = State.STANDBY;
+            }
+            else
+            {
+                Console.WriteLine("Perubahan state tidak valid");
+            }
+        }
+        public void TurnOff()
+        {
+            if (currentState == State.STANDBY)
+            {
+                Console.WriteLine("Mesin Standby Berubah menjadi Off");
+                currentState = State.OFF;
+            }
+            else
+            {
+                Console.WriteLine("Perubahan state tidak valid");
+            }
+        }
+        public void StartBrewing()
+        {
+            if (currentState == State.STANDBY)
+            {
+                Console.WriteLine("Mesin Standby berubah menjadi Brewing");
+                currentState = State.BREWING;
+            }
+            else
+            {
+                Console.WriteLine("Perubahan state tidak valid");
+            }
+        }
+        public void FinishBrewing()
+        {
+            if (currentState == State.BREWING)
+            {
+                Console.WriteLine("Mesin Brewing berubha menjadi standby");
+                currentState = State.STANDBY;
+            }
+            else
+            {
+                Console.WriteLine("Perubahan state tiadk valid");
+            }
+        }
+        public void StartMaintenance()
+        {
+            if (currentState == State.STANDBY)
+            {
+                Console.WriteLine("Mesin Standby berubah menjadi Maintenance");
+                currentState = State.MAINTENANCE;
+            }
+            else
+            {
+                Console.WriteLine("Perubahan state tidak valid");
+            }
+        }
+
+        public void EndMaintenance()
+        {
+            if (currentState == State.MAINTENANCE)
+            {
+                Console.WriteLine("Mesin Maintenance berubah menjadi Standby");
+                currentState = State.STANDBY;
+            }
+            else
+            {
+                Console.WriteLine("Perubahan state tidak valid");
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,37 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using Modul4_103022400076;
+
+KodePaket kp = new KodePaket();
+
+string[] daftarPaket = {"Basic",
+                        "Standard",
+                        "Premium",
+                        "Unlimited",
+                        "Gaming",
+                        "Streaming",
+                        "Family",
+                        "Business",
+                        "Student",
+                        "Traveler"};
+Console.WriteLine("--- Kode Paket ---");
+foreach (String paket in daftarPaket)
+{
+    Console.WriteLine($"Nama Paket: {paket} -> kode: {kp.getKodePaket(paket)}");
+}
+
+Console.WriteLine("\n --- SImulasi mesin kopi --- ");
+MesinKopi mesin = new MesinKopi();
+Console.WriteLine($"State awal: {mesin.GetState()}");
+
+mesin.TurnOn();
+mesin.StartBrewing();
+mesin.FinishBrewing();
+mesin.StartMaintenance();
+mesin.EndMaintenance();
+mesin.TurnOff(); 
+
+Console.WriteLine("\n--- Uji perubahan tidak Valid ---");
+mesin.StartBrewing();
+mesin.TurnOn();
+mesin.TurnOff();
+
